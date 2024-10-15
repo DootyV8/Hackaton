58cb829168a6ae36aa29606bc213a3d0997bdfd1
DootyV8 <163028097+DootyV8@users.noreply.github.com>
2024-10-15 12:56:10 +0200

    added confidance and grayscale

--- Stickman.py
+++ Stickman.py
@@ -8,7 +8,8 @@ import win32api, win32con
 while True:
     try:
         # Try to locate the image on screen
-        stickman_location = pyautogui.locateOnScreen('stickman.png')
+        stickman_location = pyautogui.locateOnScreen('stickman.png',confidence=0.8,grayscale=True)
+        # grayscale only if there's no vibrant colors
 
         if stickman_location is not None:
             print("I can see")
