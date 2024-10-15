cfe1f64922d785b2a8218a6fff2d13a9c971a300
DootyV8 <163028097+DootyV8@users.noreply.github.com>
2024-10-15 13:43:23 +0200

    Create Aimbot.py

---
+++ Aimbot.py
@@ -0,0 +1,35 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+time.sleep(2)
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+#Color of center: (255, 87, 34)
+
+while keyboard.is_pressed('q') == False:
+    flag = 0
+    pic = pyautogui.screenshot(region=(554, 310, 800, 550))
+
+    width, height = pic.size
+
+    for x in range(0, width, 5):
+        for y in range(0, height, 5):
+
+            r, g, b = pic.getpixel((x, y))
+
+            if b == 34 and r == 255 and g == 87:
+                flag = 1
+                click(x+554, y+310)
+                time.sleep(0.01)
+                break
+
+        if flag == 1:
+            break
